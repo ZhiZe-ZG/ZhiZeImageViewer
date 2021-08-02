db7cbfe4fa7eaca71ae76659ab2d20aabfefdf30
ZhiZe <qi_wang_zz@outlook.com>
2021-08-02 15:35:18 +0800

    formatting

--- src/Algorithms.py
+++ src/Algorithms.py
@@ -3,14 +3,14 @@ import cv2 as cv
 
 
 def RGB2BGR(image: np.ndarray) -> np.ndarray:
-    '''
+    """
     convert RGB image to bgr image
-    '''
+    """
     return cv.cvtColor(image, cv.COLOR_RGB2BGR)
 
 
 def RGB2GRAY(image: np.ndarray) -> np.ndarray:
-    '''
+    """
     convert RGB to gray image
-    '''
+    """
     return cv.cvtColor(image, cv.COLOR_RGB2GRAY)
